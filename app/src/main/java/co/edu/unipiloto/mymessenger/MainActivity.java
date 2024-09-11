825dcdb149e855f503eba864e836c7c0396af3d1
JuanCRios03 <juancjunior2010@gmail.com>
2024-09-11 11:57:00 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/mymessenger/MainActivity.java
@@ -0,0 +1,49 @@
+package co.edu.unipiloto.mymessenger;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView mensajeTextView;
+    EditText textoDeEntrada;
+    Button enviar;
+
+
+    public void buttonPress(View view){
+
+        Log.i("Info: ", "Boton presionado");
+        textoDeEntrada.setVisibility(View.INVISIBLE);
+        mensajeTextView.setText(textoDeEntrada.getText());
+        enviar.setVisibility(View.INVISIBLE);
+
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mensajeTextView = findViewById(R.id.textView);
+        mensajeTextView.setText("Esperando mensaje :)");
+        textoDeEntrada = findViewById(R.id.editarText);
+        enviar = findViewById(R.id.btn);
+    }
+}
